b4d80b798b13a6a279ab667565bd615318f78e95
xueyunqing <879634971@qq.com>
2020-10-04 19:37:17 +0800

    集成喜马拉雅sdk

--- app/src/main/java/com/example/himalaya/MainActivity.java
+++ app/src/main/java/com/example/himalaya/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.himalaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList categoryList) {
+                List<Category> category= categoryList.getCategories();
+                for (Category c: category){
+                    Log.d(TAG,"------->"+c.getCategoryName());
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.d(TAG,"------->");
+            }
+    });
     }
 }
